210118bb75e8296ea39f21f7888a83f4dc8da7a2
Julian Hoyos <jhoyos135@gmail.com>
2018-12-01 17:47:38 -0500

    fix issue

--- config/keys.js
+++ config/keys.js
@@ -1,9 +1,9 @@
 if(process.env.NODE_ENV === 'production') {
 
-    module.export = require('./prod');
+    module.exports = require('./prod');
 
 } else {
 
-    module.export = require('./dev');
+    module.exports = require('./dev');
 
 }

7d4e04d9111df37503de78eeb47961fd9e39a875
Julian Hoyos <jhoyos135@gmail.com>
2018-12-01 17:25:58 -0500

    fixed client secret

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     googleClientID: 'process.env.GOOGLE_CLIENT_ID',
-    googleClientSecret: 'process.env.env.GOOGLE_CLIENT_SECRET',
+    googleClientSecret: 'process.env.GOOGLE_CLIENT_SECRET',
     mongoURI: 'process.env.MONGO_URI',
     cookieKey: 'process.env.COOKIE_KEY'
 };
